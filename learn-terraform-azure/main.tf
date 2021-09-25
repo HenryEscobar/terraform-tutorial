89e4c1af3a54a82d7ded830a23dd94b24941569c
Henry Escobar <henry.escobar@gmail.com>
2021-09-25 11:43:53 -0400

    .

--- learn-terraform-azure/main.tf
+++ learn-terraform-azure/main.tf
@@ -17,12 +17,16 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "westus2"
+  tags = {
+    Enviroment = "Terraform Getting Started"
+    Team       = "Devops/Henry"
+  }
 }
 
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
-    name                = "myTFVnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = "westus2"
-    resource_group_name = azurerm_resource_group.rg.name
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
 }
